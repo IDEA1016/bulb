a540d234896cddc8c4fc80b0db0efb15645a4ed2
IDEA1016 <99717493+IDEA1016@users.noreply.github.com>
2022-03-15 18:22:48 +0900

    add onoff and bulb moving
    Now, the bulb moves up & down

--- img/index.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector(".toggle");
-const toggleSwitch = document.querySelector(".toggle-switch");
-
-toggleSwitch.addEventListener("mousedown", function(){
-    toggle.classList.toggle("toggleOn");
-})
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const toggle = document.querySelector(".toggle");
 const toggleSwitch = document.querySelector(".toggle-switch");
 
+const bulb = document.querySelector(".bulb-container");
+
 toggleSwitch.addEventListener("mousedown", function(){
     toggle.classList.toggle("toggleOn");
+    bulb.classList.toggle("bulb-container-on")
 })
